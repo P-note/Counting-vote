a398c6a0ade7e0f2512d1147b780f3df4a915310
P-note <65222580+P-note@users.noreply.github.com>
2023-02-15 21:14:50 +0900

    first upload
    For now, this is just an website that changes shogun's face depending on whether you click 'yes' or 'no' button.

---
+++ images.js
@@ -0,0 +1,7 @@
+function setShogunMood(mood){
+    if(mood == "happy"){
+        document.getElementById('shogunsama').src="Icon_Emoji_Raiden_Shogun_3.png";
+    }
+    else
+        document.getElementById('shogunsama').src="Icon_Emoji_Raiden_Shogun_6.png";
+}

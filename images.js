3629df47c1a8a04b30cde14836b61e1739120d43
P-note <65222580+P-note@users.noreply.github.com>
2023-02-15 22:56:17 +0900

    iframe(youtube) + banner image addition
    and also, background image changes with yes or no button clicks.
    next target, want to count the number of yes clicks for the page.

--- images.js
+++ images.js
@@ -1,7 +1,13 @@
 function setShogunMood(mood){
-    if(mood == "happy"){
+    if(mood === "happy"){
         document.getElementById('shogunsama').src="Icon_Emoji_Raiden_Shogun_3.png";
+        document.querySelector('body').style.background="url('inazuma_region.jpg')";
+        alert('(｡•̀ᴗ-)✧');
     }
-    else
+    else{
         document.getElementById('shogunsama').src="Icon_Emoji_Raiden_Shogun_6.png";
+        document.querySelector('body').style.background="url('inazuma_night.jpg')";
+        alert('凸(⊙▂⊙✖ ) 凸(⊙▂⊙✖ )');
+    }
+        
 }
